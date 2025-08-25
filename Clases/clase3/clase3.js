421092daaf0c68bb11782f33569c3986bad3701d
Maria lujan llado <mallado@itba.edu.ar>
2025-08-25 20:37:49 -0300

    Add initial HTML structure and content for clase3 index page
    - Created a new index.html file with basic HTML5 structure.
    - Included meta tags for character set and viewport for responsive design.
    - Added a header with a main heading and a paragraph of placeholder text.
    - Inserted images with appropriate alt attributes for accessibility.
    - Added comments to explain the structure and best practices for HTML.
    - Included a script tag for external JavaScript file at the end of the body.

---
+++ Clases/clase3/clase3.js
@@ -0,0 +1,6 @@
+/* Clase de HTML (lenguaje de mercado de hipertexto)
+Es donde va a estar la estructura y contenido de la pagina web */
+
+// El primer archivo si o si se tiene que llamar index.html
+
+console.log("Hola mundo");
